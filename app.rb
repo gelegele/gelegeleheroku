f0d07c1e1e8660a0169ce194f56ae53e1cfc0fb2
ryoishibashi <ryo500209@gmail.com>
2012-05-07 13:15:31 +0900

    'genba'

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
+require 'rack/rewrite'
 
-get '/' do
-  'Heroku Sinatra .. Hello world!'
+class App < Sinatra::Base
+  enable :inline_templates
+  #enable :logging
+
+  #http://localhost:4567/name/foo ‚Æ http://localhost:4567/song_for/foo ‚Å“¯‚¶‰æ–Ê
+  use Rack::Rewrite do
+    rewrite %r{^/song_for/(.*)}, '/name/$1'
+  end
+
+  get '/' do
+     @title = "Top"
+     haml "My Way"
+  end
+
+  get '/name/:name' do
+    @name = params[:name]
+    @title = "Song for #{@name}"
+    haml "#{@name}'s Way"
+  end
+
 end
+
+App.run!
+
+__END__
+
+@@ layout
+!!! 5
+%html
+ %head
+  %title= @title
+ %body
+  %h1= @title
+  %div= yield
+  aaaaa
+
